3d2c0c0690eb0f6e4ec7668d266253ade358b4e1
Your Namedukep <dukepato2010@outlook.com>
2022-05-08 23:14:42 +0100

    NFT Project Commit

---
+++ OneDrive/Documents/NFT_Collection_Ojo/hardhat-tutorial/contracts/CryptoDevs.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable  {
+    string _baseTokenURI;
+    
+    // _price of one crypto Dev NFT
+    uint256 public _price = 0.01 ether;
+
+    // _paused is used to pause the smart contract in case of an emergency
+
+    bool public _paused;
+
+    //max number of CryptoDevs
+    uint256 public maxTokensIds = 20;
+
+    // total number of tokenIds minted
+    uint256 public tokensIds;
+
+    // IWhitelist contract instance
+
+    IWhitelist whitelist;
+
+    // boolean to keep track of the presale started or not
+    bool public presaleStarted;
+
+    // timestamp for when presale would end
+    uint256 public presaleEnded;
+
+    modifier onlyWhenNotPaused {
+        require(!_paused, "Contract is currently paused");
+        _;
+    }
+
+    /**
+    * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+    * name in our case is `Crypto Devs` and symbol is `CD`.
+    * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
+    * It also initializes an instance of whitelist interface.
+    */ 
+
+    constructor (string memory baseURI, address whitelistContract) ERC721("Crypto Devs", "CD") {
+        _baseTokenURI = baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+
+    // @dev startPresale starts a presale for the whitelisted addresses
+    // startPresale
+    function startPresale() public onlyOwner {
+        require(!presaleStarted, "Presale already started!");
+        presaleStarted = true;
+
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+    // @dev presaleMint allows a user to mint one NFT per transaction during presale.abi
+
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp < presaleEnded, "Presale is not running");
+        require(whitelist.whitelistedAddresses(msg.sender), "You are not whitelisted");
+        require(tokensIds < maxTokensIds, "Exceeded maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether Sent is not correct");
+        tokensIds += 1;
+        _safeMint(msg.sender, tokensIds);
+    }
+
+    function mint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp >= presaleEnded, "Presale has not ended");
+        require(tokensIds < maxTokensIds, "Exceeded maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether Sent is not correct");
+        tokensIds += 1;
+        _safeMint(msg.sender, tokensIds);
+    }
+
+    /**
+      * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
+      * returned an empty string for the baseURI
+      */
+      function _baseURI() internal view virtual override returns (string memory) {
+          return _baseTokenURI;
+      }
+
+      /**
+      * @dev setPaused makes the contract paused or unpaused
+       */
+      function setPaused(bool val) public onlyOwner {
+          _paused = val;
+      }
+
+      /**
+      * @dev withdraw sends all the ether in the contract
+      * to the owner of the contract
+       */
+      function withdraw() public onlyOwner  {
+          address _owner = owner();
+          uint256 amount = address(this).balance;
+          (bool sent, ) =  _owner.call{value: amount}("");
+          require(sent, "Failed to send Ether");
+      }
+
+       // Function to receive Ether. msg.data must be empty
+      receive() external payable {}
+
+      // Fallback function is called when msg.data is not empty
+      fallback() external payable {}
+
+    
+
+
+
+
+
+}
---
+++ OneDrive/Documents/NFT_Collection_Ojo/my-app/pages/index.js
@@ -0,0 +1,382 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { providers, Contract, utils } from 'ethers';
+import { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import { NFT_CONTRACT_ABI, NFT_CONTRACT_ADDRESS } from '../constants';
+
+export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [presaleStarted, setPresaleStarted] = useState(false);
+  const [presaleEnded, setPresaleEnded] = useState(false);
+  const [loading, setLoading] = useState(false)
+  const [isOwner, setIsOwner] = useState(false);
+  const [numTokensMinted, setNumTokensMinted] = useState("");
+  const [totalMintable, setTotalMintable] = useState("");
+
+  const web3ModalRef = useRef();
+
+  const totalMintableTokens = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS, 
+        NFT_CONTRACT_ABI, 
+        provider
+        );
+      const maxTokenIds = await nftContract.maxTokensIds();
+      setTotalMintable(maxTokenIds.toString());
+
+    } catch (error) {
+      console.error(error)
+
+      return false;
+    }
+  }
+
+  const getNumMintedTokens = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS, 
+        NFT_CONTRACT_ABI, 
+        provider
+        );
+
+      const numTokenIds = await nftContract.tokensIds();
+      setNumTokensMinted(numTokenIds.toString());
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
+
+
+  const presaleMint = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS, 
+        NFT_CONTRACT_ABI, 
+        signer
+        );
+
+        const txn = await nftContract.presaleMint({
+          value: utils.parseEther("0.01")
+        });
+        setLoading(true);
+
+        await txn.wait();
+        setLoading(false);
+
+        window.alert("You successfully minted a cryptoDev in presale!")
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const publicMint = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS, 
+        NFT_CONTRACT_ABI, 
+        signer
+        );
+
+        const txn = await nftContract.mint({
+          value: utils.parseEther("0.015")
+        });
+        setLoading(true);
+        await txn.wait();
+        setLoading(false);
+        window.alert("You successfully minted a cryptoDev!")
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const getOwner = async() => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS, 
+        NFT_CONTRACT_ABI, 
+        signer
+        );
+
+        const owner = await nftContract.owner();
+        const userAddress = await signer.getAddress();
+
+        if (owner.toLowerCase() === userAddress.toLowerCase()) {
+          setIsOwner(true);
+        }
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const startPresale = async() => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS, 
+        NFT_CONTRACT_ABI, 
+        signer
+        );
+
+        const txn = await nftContract.startPresale();
+        setLoading(true);
+        await txn.wait();
+        setLoading(false);
+        setPresaleStarted(true);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const checkIfPresaleStarted = async () => {
+    try {
+
+      const provider = await getProviderOrSigner();
+      // Get an instance of the NFT Contract
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS, 
+        NFT_CONTRACT_ABI, 
+        provider
+        );
+
+        const isPresaleStarted = await nftContract.presaleStarted();
+        setPresaleStarted(isPresaleStarted);
+        return isPresaleStarted;
+    } catch (error) {
+      console.error(error)
+      return false;
+    }
+  };
+
+  const checkIfPresaleEnded = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      // Get an instance of the NFT Contract
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS, 
+        NFT_CONTRACT_ABI, 
+        provider
+        );
+        // This will return a big Number because of Uint256 time stamp in sec
+        const presaleEndTime = await nftContract.presaleEnded();
+        const currentTimeInSeconds = Date.now() / 1000;
+        const hasPresaleEnded = presaleEndTime.lt(
+          Math.floor(currentTimeInSeconds)
+          );
+
+          if (hasPresaleEnded) {
+            setPresaleEnded(true);
+
+          }
+          else {
+            setPresaleEnded(false);
+          }
+          return hasPresaleEnded;
+
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+
+  const connectWallet = async() => {
+    try {
+      await getProviderOrSigner();
+    setWalletConnected(true);
+
+    // Update 'walletConnected' to be true
+    } catch (error) {
+      console.error(error);
+    }
+    
+  };
+
+  const getProviderOrSigner = async (needSigner = false) => {
+  
+    // We need to gain access to the provider/signer from metamask
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If the user is not connected to Rinkeby, tell them to switch to rinkeby
+
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Please switch to the Rinkeby network");
+      throw new Error("Incorrect network")
+    };
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+
+    return web3Provider;
+  };
+
+  const onPageLoad = async () => {
+    await connectWallet();
+    await getOwner();
+    const _presaleStarted = await checkIfPresaleStarted();
+    if (_presaleStarted) {
+      await checkIfPresaleEnded();
+    }
+
+    await getNumMintedTokens();
+
+    await totalMintableTokens();
+
+      // Track inn realtime the numbers of minted NFTs
+      //Track in realtime the status of sales
+
+    setInterval(async() => {
+      await getNumMintedTokens()
+    }, 5 * 1000);
+
+    setInterval(async() => {
+      const _presaleStarted = await checkIfPresaleStarted();
+      if (_presaleStarted) {
+        await checkIfPresaleEnded()
+      } 
+    }, 5 * 1000);
+  }
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+
+     onPageLoad();
+      
+    }
+
+  }, [walletConnected]);
+
+  function renderBody() {
+    if (!walletConnected) {
+      return (
+        <button onClick={connectWallet} className={styles.button}> Connect your Wallet</button>
+      );
+    }
+
+    if (loading) {
+      return (
+        <div className={styles.description}>
+          Loading...
+        </div>
+      )
+    }
+
+    if (isOwner && !presaleStarted) {
+      // render a button to start the presale
+      return (
+        <button onClick={startPresale} className={styles.button}>
+          Start Presale
+        </button>
+      );
+      }
+
+      if (!isOwner && !presaleStarted){
+        return (
+          <div>
+            <div className={styles.description}>
+            Presale has not started yet check back later
+            </div>
+            
+          </div>
+        );
+      }
+
+      if (presaleStarted && !presaleEnded) {
+        // allow users to mint in presale
+        // they need to be in whitelist for this to work
+        return (
+          <div>
+            <span className={styles.description}>
+              Presale has started! If your address is whitelisted, you can mint a Crypto Dev!
+            </span>
+
+            <button onClick={presaleMint} className={styles.button}>
+              Presale Mint 🚀
+            </button>
+          </div>
+        );
+       
+      }
+
+      if (presaleEnded) {
+        // allow users to take part in public mint sale
+        return(
+          <div>
+          <div className={styles.description}>
+            Presale has ended. You can mint a Crypto Dev in public sale if any remains
+          </div>
+          
+          <div className={styles.description}>
+               { numTokensMinted }/ {totalMintable} NFTs have been minted
+          </div>
+
+          <button onClick={publicMint} className={styles.button}>
+            Public Mint 🚀
+          </button>
+
+         
+        </div>
+        );
+        
+       
+      }
+
+      return (
+        <div>
+            {numTokensMinted}/{totalMintableTokens()}
+          </div>
+      );
+  }
+
+  return ( 
+    <div className={styles.main}> 
+      <Head>
+        <title>Crypto Devs NFT</title>
+      </Head>
+
+      <div>
+
+        <div>
+          <h1 className={styles.title}>Welcome to CryptoDevs NFT</h1>
+          <div className={styles.description}>
+            CryptoDevs is a collection for web3 developers
+          </div>
+          
+          <div>
+          {renderBody()}
+          </div>
+        </div>
+        <img className={styles.image} src="/cryptodevs/0.svg" />
+      </div>
+
+      <div>
+        <footer>
+          Made with &#10084; by CryptoDevs DHC (c) 2022
+        </footer>
+      </div>
+    
+    </div>
+  );
+}
+
