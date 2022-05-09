8619c4fae96dc9b96cb9be91ad846f475470db86
Your Namedukep <dukepato2010@outlook.com>
2022-05-09 10:43:10 +0100

    addition of api endpoint

--- OneDrive/Documents/NFT_Collection_Ojo/hardhat-tutorial/constants/index.js
+++ OneDrive/Documents/NFT_Collection_Ojo/hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x6bCaCd5bfce341a74a31e047B7BFe1a04298F2F4";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://cryptodevs-nft-collection-hazel.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
